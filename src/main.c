85fd2c04099c10e2e82dde8d70ad65239c84c23c
abvee <icaliberdev@gmail.com>
2025-09-05 14:34:22 +0530

    prompt

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 int lineread();
+int print(char *s);
 
 enum {stdIn, stdOut, stdErr}; // fds
 enum {MLEN = 1024}; // max length of a line
@@ -13,8 +14,10 @@ static char line[MLEN];
 int main(int argc, char *argv[]) {
 
 	// stdin read loop
+	print("> ");
 	while (lineread()) {
 		printf("%s\n", line);
+		print("> ");
 	}
 	return 0;
 }
@@ -27,4 +30,9 @@ int lineread() {
 		return 0;
 	line[n-1] = '\0'; // strip newline
 	return n-1; // return last index, not length
+}
+
+// unbuffered print
+int print(char *s) {
+	return write(stdOut, s, strlen(s));
 }
