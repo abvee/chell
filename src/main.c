ff29a510a1450748c179f84027f9066d78148ba6
abvee <icaliberdev@gmail.com>
2025-09-05 19:20:00 +0530

    add very basic execlp. No arguments yet

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int lineread();
 int print(char *s);
+int tok(char const *line, char *buf);
 
 enum {stdIn, stdOut, stdErr}; // fds
 enum {MLEN = 1024}; // max length of a line
@@ -12,11 +14,26 @@ enum {MLEN = 1024}; // max length of a line
 static char line[MLEN];
 
 int main(int argc, char *argv[]) {
+	// TODO: don't output this when you're running a shell script
+	print("> ");
+
+	char root_command[1024] = {};
+	int exit_val = 0; // return value of the previous command
 
 	// stdin read loop
-	print("> ");
 	while (lineread()) {
-		printf("%s\n", line);
+		// first token is the root token
+		// TODO: This should reset when we find a pipe
+		tok(line, root_command);
+
+		switch (fork()) {
+			case 0: // child
+				execlp(root_command, root_command, NULL); // TODO: replace with execv
+			default:
+				wait(&exit_val);
+		}
+		// TODO: make the prompt a bit more dynamic, so it shows exit values
+		// print("> ", ret_val);
 		print("> ");
 	}
 	return 0;
@@ -35,4 +52,20 @@ int lineread() {
 // unbuffered print
 int print(char *s) {
 	return write(stdOut, s, strlen(s));
+}
+
+// return length of buffer. Do the checks for line and buffer length before you
+// enter this function
+
+// NOTE: we don't handle newlines here. Assume line doesn't have a new line
+int tok(char const *line, char *buf) {
+	int li = 0, bufi = 0; // indexes
+	while (line[li] == ' ' || line[li]  == '\t' && line[li])
+		li++;
+
+	while ((buf[bufi] = line[li]) != '\t' && buf[bufi] != ' ' && buf[bufi] != '\0')
+		bufi++, li++;
+
+	buf[bufi] = '\0'; // account for the whitespace
+	return bufi;
 }
