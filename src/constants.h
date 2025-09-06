5d5a81d83953cd31fd0c4d78eac0d87c8fd46167
abvee <icaliberdev@gmail.com>
2025-09-06 11:53:40 +0530

    split into multiple files

---
+++ src/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS
+enum {stdIn, stdOut, stdErr}; // fds
+
+// Line length, max argument length
+enum {MLEN = 1024, MARGS = 64};
+#define CONSTANTS 
+#endif
---
+++ src/parser.c
@@ -0,0 +1,31 @@
+#include "constants.h"
+#include <unistd.h>
+
+static char line[MLEN];
+static int li; // line index
+
+// read into line, return number of characters read
+int lineread() {
+	int n = read(stdIn, line, MLEN);
+	// bounds checking
+	if (n >= MLEN)
+		return 0;
+	line[n-1] = '\0'; // strip newline
+	return n-1; // return last index, not length
+}
+
+// return length of buffer. Do the checks for line and buffer length before you
+// enter this function
+
+// NOTE: we don't handle newlines here. Assume line doesn't have a new line
+int tok(char *buf) {
+	int bufi = 0; // indexes
+	while (line[li] && (line[li] == ' ' || line[li]  == '\t'))
+		li++;
+
+	while ((buf[bufi] = line[li]) != '\t' && buf[bufi] != ' ' && buf[bufi] != '\0')
+		bufi++, li++;
+
+	buf[bufi] = '\0'; // account for the whitespace
+	return bufi;
+}
