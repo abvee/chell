7e349a9310d7af4392fd0be3f72ec0297834e94e
abvee <icaliberdev@gmail.com>
2025-09-06 11:54:19 +0530

    change to use new parser and constants file. Also set up the base for argv when moving to execv

--- src/main.c
+++ src/main.c
@@ -4,31 +4,42 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int lineread();
+// TODO: something something header files, something something external linkage
+#include "constants.h"
+#include "parser.c"
+
 int print(char *s);
-int tok(char const *line, char *buf);
 
-enum {stdIn, stdOut, stdErr}; // fds
-enum {MLEN = 1024}; // max length of a line
-
-static char line[MLEN];
+char root_command[MLEN]; // buffer for root command
+char args_buf[MARGS][MLEN]; // buffer arguments for command
 
 int main(int argc, char *argv[]) {
+
+	int exit_val = 0; // return value of the previous command
+
+	char *args[MARGS] = {root_command, NULL};
+	// set every pointer in args to the args_buf. 
+	for (int i = 1; i < MARGS; i++)
+		args[i] = (char *)(args_buf + i);
+	// NOTE: this is basically a long way around doing malloc
+
 	// TODO: don't output this when you're running a shell script
 	print("> ");
-
-	char root_command[1024] = {};
-	int exit_val = 0; // return value of the previous command
 
 	// stdin read loop
 	while (lineread()) {
 		// first token is the root token
 		// TODO: This should reset when we find a pipe
-		tok(line, root_command);
+		tok(root_command);
 
 		switch (fork()) {
 			case 0: // child
+				// the rest of the arguments are pointers to args
 				execlp(root_command, root_command, NULL); // TODO: replace with execv
+
+				// it's an error if we get here
+				fprintf(stderr, "Command not found");
+
 			default:
 				wait(&exit_val);
 		}
@@ -39,33 +50,7 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-// read into line, return number of characters read
-int lineread() {
-	int n = read(stdIn, line, MLEN);
-	// bounds checking
-	if (n >= MLEN)
-		return 0;
-	line[n-1] = '\0'; // strip newline
-	return n-1; // return last index, not length
-}
-
 // unbuffered print
 int print(char *s) {
 	return write(stdOut, s, strlen(s));
-}
-
-// return length of buffer. Do the checks for line and buffer length before you
-// enter this function
-
-// NOTE: we don't handle newlines here. Assume line doesn't have a new line
-int tok(char const *line, char *buf) {
-	int li = 0, bufi = 0; // indexes
-	while (line[li] == ' ' || line[li]  == '\t' && line[li])
-		li++;
-
-	while ((buf[bufi] = line[li]) != '\t' && buf[bufi] != ' ' && buf[bufi] != '\0')
-		bufi++, li++;
-
-	buf[bufi] = '\0'; // account for the whitespace
-	return bufi;
 }
